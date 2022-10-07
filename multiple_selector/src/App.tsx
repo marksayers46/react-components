b259a861e99295bfece81d822d6210ba00fefa23
Mark Sayers <marksayers46@icloud.com>
2022-10-06 21:05:15 -0500

    cleaning up files not needed

--- multiple_selector/src/App.tsx
+++ multiple_selector/src/App.tsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
 import { Select } from "./Select"
 
+const options = [
+  {
+    label: "First", 
+    value: 1
+  },
+  {
+    label: "Second",
+    value: 2
+  },
+  {
+    label: "Third",
+    value: 3
+  },
+  {  
+    label: "fourth",
+    value: 4
+  },
+  {
+    label: "fifth",
+    value: 5
+  },  
+]
+
 function App() {
 
   return (
     <>
-      <Select />
+      <Select options={options}/>
     </>
   )
 }
